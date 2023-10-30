31e14c5d62402b5d445011fbcc0def4fcc6d9d6b
Kestas <kestutisjankevicius9@gmail.com>
2023-10-30 16:14:58 +0200

    Create form done

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -5,13 +5,34 @@
 
     <form method="POST" action="/users">
         @csrf
+        <!-- Name -->
         <div class="mb-6">
             <label for="name" class="inline-block text-lg mb-2"> Name</label>
             <input type="text" class="border border-gray-100 rounded p-2 w-full" name="name" value="{{old('name')}}"/>
         </div>
-
+        <!-- Email -->
         <div class="mb-6">
             <label for="email" class="inline-block text-lg mb-2">Email</label>
             <input type="email" class="border border-gray-100 rounded p-2 w-full" name="email" value="{{old('email')}}">
         </div>
+        <!-- Password -->
+        <div class="mb-6">
+            <label for="password" class="inline-block text-lg mb-2">Password</label>
+            <input type="password" class="border border-gray-100 rounded p-2 w-full" name="password" value="{{old('password')}}">
+        </div>
+        <!-- Password confirmation -->
+        <div class="mb-6">
+            <label for="password2">Confirm Password</label>
+            <input type="password" class="border border-gray-100 rounded p-2 w-full" name="password_confirmation" value="{{old('password_confirmation')}}">
+        </div>
+        <!-- Sign up button -->
+        <div class="mb-6">
+            <button type="submit" class="text-black py-2 px-4 hover:bg-green">Sign up</button>
+        </div>
+        <!-- Login link -->
+        <div class="mb-6">
+            <p>Already have an account?</p>
+            <a href="/login" class="text-laravel">Login</a>
+        </div>
+
 </x-layout>
