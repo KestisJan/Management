64783a5c2c7ba6be3ecbc2fea697775b9204657c
Kestas <kestutisjankevicius9@gmail.com>
2023-10-31 15:14:15 +0200

    Admin index update v2

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,7 +3,10 @@
         <table class="min-w-full divide-y divide-gray-200">
             <thead>
                 <tr>
-                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Header 1</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Functions</th>
                 </tr>
             </thead>
             <tbody>
@@ -11,6 +14,12 @@
                 <tr class="bg-white">
                     <td class="px-6 py-4 whitespace-nowrap">{{ $user->name }}</td>
                     <td class="px-6 py-4 whitespace-nowrap">{{ $user->email }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $user->role }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <a href="#" class="text-blue-500 hover:underline">Edit</a>
+                        <a href="#" class="text-red-500 hover:underline">Delete</a>
+                    </td>
+                    
                 </tr>
                 @endforeach
             </tbody>
