9ec6d786210ffce0f883898c4003754f6e4c7da2
Kestas <kestutisjankevicius9@gmail.com>
2023-10-31 15:09:54 +0200

    Admin index update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,4 +1,19 @@
 <x-layout>
     <x-container>
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead>
+                <tr>
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Header 1</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                <tr class="bg-white">
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $user->name }}</td>
+                    <td class="px-6 py-4 whitespace-nowrap">{{ $user->email }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </x-container>
 </x-layout>
